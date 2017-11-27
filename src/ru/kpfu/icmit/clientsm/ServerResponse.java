4410c69da180b79a427f410a2e21f7d48cfe54a7
ksenikeev <ksenikeev@mail.ru>
2017-11-27 13:18:05 +0300

    Response

---
+++ src/ru/kpfu/icmit/clientsm/ServerResponse.java
@@ -0,0 +1,9 @@
+package ru.kpfu.icmit.clientsm;
+
+public class ServerResponse {
+    int responseCode;
+    String responseDescription;
+    int contentLength;
+    String contentType;
+    String content;
+}
