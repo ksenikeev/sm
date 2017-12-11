094eb5b013a8f481d6a0cf7a62e87c25b91b85ac
ksenikeev <ksenikeev@mail.ru>
2017-12-11 21:38:44 +0300

    Reg user

--- src/ru/kpfu/icmit/clientsm/Registration.java
+++ src/ru/kpfu/icmit/clientsm/Registration.java
@@ -3,20 +3,28 @@ package ru.kpfu.icmit.clientsm;
 import com.google.gson.Gson;
 import java.util.ArrayList;
 
+/**
+ * Класс отрабатывает регистрацию пользователя на стороне клиента
+ */
 public class Registration {
 
+    //Отправка регистрационных данных и прием ответа от сервера
     public static ServerResponse sendRegInfo(String login, String password){
 
         System.out.println("Start registration. Username "+login);
-
+        //Формирование JSON объекта который будет отправлен на сервер
         String message = "{\"name\":\""+login+"\",\"login\":\""+login+"\",\"password\":\""+password+"\"}";
+
         HTTPSender sender = new HTTPSender("127.0.0.1", "3128");
+        //Отправляем сообщение message на сервер с указанием ресурса "/reguser" который должен обработать наше сообщение
         ServerResponse resp = sender.sendMessage(message, "/reguser");
 
         System.out.println("Response code: "+resp.responseCode);
+        // Возвращаем ответ сервера для дальнейшего анализа
         return resp;
     }
 
+    //После успешной регистрации запрашиваем список всех абонентов системы (чтобы было кому писать)
     public static ArrayList<Abonent> getAbonentList(String token){
         System.out.println("Request abonent list");
         String message = "{\"token\":\""+token+"\"}";
