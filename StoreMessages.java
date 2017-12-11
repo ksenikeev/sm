72c57c15e9ca4b8df146fce8d56bb0cd4e297cb8
GabutdinovaDinara <34024060+GabutdinovaDinara@users.noreply.github.com>
2017-12-11 12:48:42 +0300

    Add files via upload

---
+++ StoreMessages.java
@@ -0,0 +1,54 @@
+package ru.kpfu.icmit.clientsm;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+/**
+ * Класс реализует:
+ * 1) функции по сохранению отправленных и прочитанных сообщений в файл.
+ * 2) функции по считыванию отправленных и прочитанных сообщений из файла.
+ *
+ * Формат файла: текстовый файл в кодировке UTF-8.
+ * Каждая строка содержит отдельное сообщение в виде JSON объекта
+ * {"from":{"id":"","name":""}, "to":{"id":"","name":""}, "content":"", "datemsg":"yyyy-MM-dd HH:mm:ss", "status":""}
+   TODO необходимо подготовить класс, моделирующий такой JSON объект (прототип ClientMessage)
+ */
+public class StoreMessages {
+
+    /** Метод для чтения сохраненных сообщений из файла */
+    public static List<ClientMessage> readMessages(String fileName){
+        //TODO реализовать
+        return null;
+    }
+
+
+    /** Метод для записи сообщений в файл 
+     * @param <JSONObject>*/
+   	
+    public static void writeMessages(List<ClientMessage> msgs, String fileName){
+    	Gson gson = new Gson();
+        
+    	try (FileWriter writer = new FileWriter("history.joc")){
+    		for (ClientMessage cm : msgs) { 
+            String msg = gson.toJson(cm);
+    			writer.write(msg+"\n");
+        
+            }
+            writer.flush();
+         
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } 
+      //TODO реализовать 
+    }
+    	
+  
+    	  /*   public static void main(String[] args) {
+    	  
+    	     } */
+}
+    	
+    
